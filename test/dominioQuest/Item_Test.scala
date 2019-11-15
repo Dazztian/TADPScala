9429ab5a54d1e6509b77d257367b2fbfc8adb31d
María Azul Grande <azulegrande1@gmail.com>
2019-11-15 16:18:24 -0300

    test de talisman minimalista

--- test/dominioQuest/Item_Test.scala
+++ test/dominioQuest/Item_Test.scala
@@ -7,6 +7,7 @@ import org.junit.Assert._
 class Item_Test {
 
 var kaerin:Heroe = null
+var kaerinRecargado :Heroe = null
 var druida:Trabajo = null
 var laBotellita:Item = null
 var manoDerecha= new ParteDelCuerpo(None)
@@ -14,31 +15,42 @@ var cabeza= new ParteDelCuerpo(None)
 var laBotellitaDeRicky:Item = null
 var palitoMagico:Item = null
 var tacosDeSarkany:Item = null
-
+var sinRequerimiento : List[RequerimientosItem] = List()
+var talismanDedicacion:Item = null
+var talismanMinimalismo: Item = null
 
   @Before
   def setup() = {
-  
+   
+   
     druida=new Trabajo(Hp, Map(Fuerza -> (1+)))
     
-     var funcionPrueba: Int=>Boolean=(30<)
-     var requi:RequerimientosItem=new RequerimientosItem(Some(druida), Map(Fuerza -> funcionPrueba))
-     var requerimientoPalitoMagico: List[RequerimientosItem]= List(requi) 
+    var funcionPrueba: Int=>Boolean=(30<)
+    var requi:RequerimientosItem=new RequerimientosItem(Some(druida), Map(Fuerza -> funcionPrueba))
+    var requerimientoPalitoMagico: List[RequerimientosItem]= List(requi) 
      
-     laBotellita=new Item(Some(Manos.derecha),Map(Hp ->(4* )), requerimientoPalitoMagico)
+    laBotellita=new Item(Some(Manos.derecha),Map(Hp ->(4* )), requerimientoPalitoMagico)
      
      //val partesDelCuerpo: List[ParteDelCuerpo]= List(Manos.derecha)
-     var listaItems: List[Item]= List(laBotellita)
-
+    var listaItems: List[Item]= List(laBotellita)
+  
     
-     kaerin = new Heroe(1,40,3,4, Some(druida), listaItems)//,partesDelCuerpo )
+    kaerin = new Heroe(1,40,3,4, Some(druida), listaItems)//,partesDelCuerpo )
      
-     
-      druida=new Trabajo(Hp, Map(Fuerza -> (1+)))
-       
-      laBotellitaDeRicky = new Item(Some(Manos.derecha),Map(Hp ->(40* )), requerimientoPalitoMagico)
-      tacosDeSarkany = new Item(Some(Pies), Map(Fuerza -> (300*)), List(new RequerimientosItem(None, Map(Inteligencia ->(3 ==)))))
-      palitoMagico = new Item(Some(Manos.izquiera),Map(Hp ->(40* )), requerimientoPalitoMagico)
+   
+    druida=new Trabajo(Hp, Map(Fuerza -> (1+)))
+    
+    var listaItemsRecargada: List[Item]= List(laBotellita,tacosDeSarkany, palitoMagico)
+
+    kaerinRecargado =new Heroe(10,40,3,4, Some(druida), listaItemsRecargada)//,partesDelCuerpo ))
+    
+    //ITEMS
+    laBotellitaDeRicky = new Item(Some(Manos.derecha),Map(Hp ->(40* )), requerimientoPalitoMagico)
+    tacosDeSarkany = new Item(Some(Pies), Map(Fuerza -> (300*)), List(new RequerimientosItem(None, Map(Inteligencia ->(3 ==)))))
+    palitoMagico = new Item(Some(Manos.izquiera),Map(Hp ->(40* )), requerimientoPalitoMagico)
+    //talismanDedicacion = new Item(None,Map(Trabajo.atributoPrincipal -> (0.1*)), requerimientoPalitoMagico) 
+   talismanMinimalismo = new Item(None, Map(Hp ->(50+)),sinRequerimiento)
+    
   }
 
   @Test
@@ -73,7 +85,14 @@ var tacosDeSarkany:Item = null
     
     assertEquals(2, kaerinConNuevoItem.items.size)
   }
-  
+ @Test
+  def cantItemPOSTEquipamientoDeTalismanes_test() = {
+   
+    var kaerinRecargadoConNuevoTalisman=kaerinRecargado.equiparItem(talismanMinimalismo) 
+
+    assertEquals(4, kaerinRecargadoConNuevoTalisman.items.size)
+  }
+    
  
   
 }
