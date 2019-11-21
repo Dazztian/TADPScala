9f58bec124b553c6f45fb5dc415e7dab81303ca1
AlvaroMaser <alvaro199x@gmail.com>
2019-11-21 01:40:44 -0300

    Empezada un poco logica de misiones

--- src/dominioQuest/Mision.scala
+++ src/dominioQuest/Mision.scala
@@ -1,7 +1,22 @@
 package dominioQuest
 
 case class Mision (
-    var recomensa: Any,
+    var recompensa: Any,
     var tareas: List[Tarea]){
+  
+  def realizarMision(unEquipo:Equipo):Equipo = { //podemos devolver un equipo o lo que sea que devuelva una recompensa, o un exception de que no logro hace la mision
+    if(realizoTodasLasTareas(unEquipo)){
+      this.recompensa(unEquipo)
+    }else{
+      null
+      //error, no pudo hacer la mision
+    }
+  }
+  
+  def realizoTodasLasTareas(unEquipo:Equipo): Boolean = {
+    tareas.foreach{tarea=> realizarTarea(tarea,tarea.heroeMasApto(unEquipo),unEquipo)} //Preguntar como hacerlo con fold
+    //porque debemos ir cambiando a la semilla (el heroe) y tambien ir cambiando la tarea
+   return true
+  }
   
 }
