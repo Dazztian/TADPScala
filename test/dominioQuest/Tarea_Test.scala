988112d4ac6a9a3f4f9602468c1d945a574072f3
Federico Hazama <fehc97@hotmail.com>
2019-11-28 17:53:28 -0300

    arreglo tarea test para que use list en lugar de map

--- test/dominioQuest/Tarea_Test.scala
+++ test/dominioQuest/Tarea_Test.scala
@@ -25,10 +25,11 @@ var sinRequerimiento : List[RequerimientosItem] = List()
 
   @Before
   def setup() = {
+
     // Trabajos
-    mago=new Mago(Inteligencia, Map(Fuerza -> (100+)) )
-    guerrero = new Guerrero(Inteligencia, Map(Fuerza -> (100+)))
-    ladron =new Ladron(Velocidad, Map(Velocidad -> (10+),Hp ->(5-)) )
+    mago=new Mago(Inteligencia, List(_.modificarFuerza(100+)) )
+    guerrero = new Guerrero(Inteligencia, List(_.modificarFuerza(100+)))
+    ladron = new Ladron(Velocidad, List(_.modificarVelocidad (10+), _.modificarHp (5-)) )
     
     // Heroes
     heroeMago = new Heroe(0, 2,100,1,1, 2,100,1,1, Some(mago),List())
@@ -39,7 +40,7 @@ var sinRequerimiento : List[RequerimientosItem] = List()
     equipo = new Equipo(0,"Equipo sin gracia",List(heroeMago, heroeGuerrero))
     estoNoEsUnEquipo = new Equipo(0,"Equipo sin nadie",List())
     
-    arcoViejo = new Item(Some(Manos),Map(Fuerza -> (2+)), sinRequerimiento,10)    
+    arcoViejo = new Item(Some(Manos),List(_.modificarFuerza( (2+))), sinRequerimiento,10)
     
   }
 
@@ -53,25 +54,4 @@ var sinRequerimiento : List[RequerimientosItem] = List()
     assertEquals(None,RobarTalisman(arcoViejo).encontrarMejorHeroe(estoNoEsUnEquipo))
   }
 
-//  @Test
-//  def equipoNoCumpleCondicionTarea()= {
-//       assertEquals(NoPuedeRealizarse(equipo),equipo.puedeRealizarTarea(RobarTalisman(arcoViejo)))
-//   }
-//  @Test
-//  def equipoCumpleCondicionTarea()= {
-//    var equipoConLiderLadron = equipo.obtenerMiembro(liderLadron)
-//     assertEquals(Success(equipoConLiderLadron),equipoConLiderLadron.puedeRealizarTarea(RobarTalisman(arcoViejo)))
-//   }
-//  @Test
-//  def equipoSinMiembrosNoCumpleTarea(){
-//    assertEquals(NoPuedeRealizarse(estoNoEsUnEquipo),estoNoEsUnEquipo.puedeRealizarTarea(RobarTalisman(arcoViejo)))
-//  }
-//  @Test
-//  def tareaSinConidcionEquipoVacio(){
-//    assertEquals(NoPuedeRealizarse(estoNoEsUnEquipo),estoNoEsUnEquipo.puedeRealizarTarea(PelearContraMonstruo(10)))
-//  }
-//  @Test
-//  def tareaSinCondicionEquipoConHeroes(){
-//     assertEquals(Success(equipo),equipo.puedeRealizarTarea(PelearContraMonstruo(10)))
-//  }
 }
